57bc0e8b7539536d2c66e770e9e105cc6e45824d
sysfi <64483657+sysfi@users.noreply.github.com>
2023-07-15 17:59:34 +0200

    test

--- examples/perplexity/perplexity.cpp
+++ examples/perplexity/perplexity.cpp
@@ -4,9 +4,9 @@
 
 #include <cmath>
 #include <ctime>
+
 #include <fstream>
-#include <vector>
-#include <string>
+#include <iostream>
 
 #if defined(_MSC_VER)
 #pragma warning(disable: 4244 4267) // possible loss of data
@@ -28,110 +28,115 @@ std::vector<float> softmax(const std::vector<float>& logits) {
     return probs;
 }
 
-std::vector<std::string> readPromptsFromFile(const std::string& filename) {
-    std::ifstream file(filename);
-    std::vector<std::string> prompts;
-    std::string line;
-    while (std::getline(file, line)) {
-        prompts.push_back(line);
+void perplexity(llama_context *ctx, const gpt_params &params, const std::string &promptsFile) {
+    std::ifstream inputFile(promptsFile);
+    if (!inputFile.is_open()) {
+        fprintf(stderr, "Failed to open prompts file: %s\n", promptsFile.c_str());
+        return;
     }
-    return prompts;
+
+    std::ofstream outputFile("/kaggle/working/perplexity.txt");
+    if (!outputFile.is_open()) {
+        fprintf(stderr, "Failed to open output file: /kaggle/working/perplexity.txt\n");
+        return;
+    }
+
+    std::string prompt;
+    while (std::getline(inputFile, prompt)) {
+        auto tokens = ::llama_tokenize(ctx, prompt, true);
+
+        int count = 0;
+
+        const int n_chunk = tokens.size() / params.n_ctx;
+        const int n_vocab = llama_n_vocab(ctx);
+        const int n_batch = params.n_batch;
+
+        double nll = 0.0;
+
+        fprintf(stderr, "%s: calculating perplexity over %d chunks, batch_size=%d\n", __func__, n_chunk, n_batch);
+
+        for (int i = 0; i < n_chunk; ++i) {
+
+        for (int i = 0; i < n_chunk; ++i) {
+            const int start =     i * params.n_ctx;
+            const int end   = start + params.n_ctx;
+
+            const int num_batches = (params.n_ctx + n_batch - 1) / n_batch;
+
+            std::vector<float> logits;
+
+            const auto t_start = std::chrono::high_resolution_clock::now();
+
+            for (int j = 0; j < num_batches; ++j) {
+                const int batch_start = start + j * n_batch;
+                const int batch_size  = std::min(end - batch_start, n_batch);
+
+                // save original token and restore it after eval
+                const auto token_org = tokens[batch_start];
+
+                // add BOS token for the first batch of each chunk
+                if (j == 0) {
+                    tokens[batch_start] = llama_token_bos();
+                }
+
+                if (llama_eval(ctx, tokens.data() + batch_start, batch_size, j * n_batch, params.n_threads)) {
+                    fprintf(stderr, "%s : failed to eval\n", __func__);
+                    return;
+                }
+
+                // restore the original token in case it was set to BOS
+                tokens[batch_start] = token_org;
+
+                const auto batch_logits = llama_get_logits(ctx);
+                logits.insert(logits.end(), batch_logits, batch_logits + batch_size * n_vocab);
+            }
+
+            const auto t_end = std::chrono::high_resolution_clock::now();
+
+            if (i == 0) {
+                const float t_total = std::chrono::duration<float>(t_end - t_start).count();
+                fprintf(stderr, "%s: %.2f seconds per pass - ETA ", __func__, t_total);
+                int total_seconds = (int)(t_total * n_chunk);
+                if (total_seconds >= 60*60) {
+                    fprintf(stderr, "%d hours ", total_seconds / (60*60));
+                    total_seconds = total_seconds % (60*60);
+                }
+                fprintf(stderr, "%d minutes\n", total_seconds / 60);
+            }
+
+            // We get the logits for all the tokens in the context window (params.n_ctx)
+            // from llama_eval above.  Now, based on https://huggingface.co/docs/transformers/perplexity,
+            // calculate the perplexity over the last half of the window (so the model always has
+            // some context to predict the token).
+            //
+            // We rely on the fact that attention in the forward pass only looks at previous
+            // tokens here, so the logits returned for each token are an accurate representation
+            // of what the model would have predicted at that point.
+            //
+            // Example, we have a context window of 512, we will compute perplexity for each of the
+            // last 256 tokens.  Then, we split the input up into context window size chunks to
+            // process the entire prompt.
+            for (int j = std::min(512, params.n_ctx / 2); j < params.n_ctx - 1; ++j) {
+                // Calculate probability of next token, given the previous ones.
+                const std::vector<float> tok_logits(
+                    logits.begin() + (j + 0) * n_vocab,
+                    logits.begin() + (j + 1) * n_vocab);
+
+                const float prob = softmax(tok_logits)[tokens[start + j + 1]];
+
+                nll += -std::log(prob);
+                ++count;
+            }
+            // perplexity is e^(average negative log-likelihood)
+            double perplexity = std::exp(nll / count);
+            outputFile << perplexity << std::endl;
+        }
+    }
+    inputFile.close();
+    outputFile.close();
 }
 
-void perplexity(llama_context* ctx, const gpt_params& params, const std::string& prompt) {
-    // Download: https://s3.amazonaws.com/research.metamind.io/wikitext/wikitext-2-raw-v1.zip?ref=salesforce-research
-    // Run `./perplexity -m models/7B/ggml-model-q4_0.bin -f wiki.test.raw`
-    // Output: `perplexity: 13.5106 [114/114]`
-    // BOS tokens will be added for each chunk before eval
-    auto tokens = ::llama_tokenize(ctx, prompt, true);
-
-    int count   = 0;
-
-    const int n_chunk = tokens.size() / params.n_ctx;
-    const int n_vocab = llama_n_vocab(ctx);
-    const int n_batch = params.n_batch;
-
-    double nll = 0.0;
-    fprintf(stderr, "%s: calculating perplexity over %d chunks, batch_size=%d\n", __func__, n_chunk, n_batch);
-
-    for (int i = 0; i < n_chunk; ++i) {
-        const int start =     i * params.n_ctx;
-        const int end   = start + params.n_ctx;
-
-        const int num_batches = (params.n_ctx + n_batch - 1) / n_batch;
-
-        std::vector<float> logits;
-
-        const auto t_start = std::chrono::high_resolution_clock::now();
-
-        for (int j = 0; j < num_batches; ++j) {
-            const int batch_start = start + j * n_batch;
-            const int batch_size  = std::min(end - batch_start, n_batch);
-
-            // save original token and restore it after eval
-            const auto token_org = tokens[batch_start];
-
-            // add BOS token for the first batch of each chunk
-            if (j == 0) {
-                tokens[batch_start] = llama_token_bos();
-            }
-
-            if (llama_eval(ctx, tokens.data() + batch_start, batch_size, j * n_batch, params.n_threads)) {
-                fprintf(stderr, "%s : failed to eval\n", __func__);
-                return;
-            }
-
-            // restore the original token in case it was set to BOS
-            tokens[batch_start] = token_org;
-
-            const auto batch_logits = llama_get_logits(ctx);
-            logits.insert(logits.end(), batch_logits, batch_logits + batch_size * n_vocab);
-        }
-
-        const auto t_end = std::chrono::high_resolution_clock::now();
-
-        if (i == 0) {
-            const float t_total = std::chrono::duration<float>(t_end - t_start).count();
-            fprintf(stderr, "%s: %.2f seconds per pass - ETA ", __func__, t_total);
-            int total_seconds = (int)(t_total * n_chunk);
-            if (total_seconds >= 60*60) {
-                fprintf(stderr, "%d hours ", total_seconds / (60*60));
-                total_seconds = total_seconds % (60*60);
-            }
-            fprintf(stderr, "%d minutes\n", total_seconds / 60);
-        }
-
-        // We get the logits for all the tokens in the context window (params.n_ctx)
-        // from llama_eval above.  Now, based on https://huggingface.co/docs/transformers/perplexity,
-        // calculate the perplexity over the last half of the window (so the model always has
-        // some context to predict the token).
-        //
-        // We rely on the fact that attention in the forward pass only looks at previous
-        // tokens here, so the logits returned for each token are an accurate representation
-        // of what the model would have predicted at that point.
-        //
-        // Example, we have a context window of 512, we will compute perplexity for each of the
-        // last 256 tokens.  Then, we split the input up into context window size chunks to
-        // process the entire prompt.
-        for (int j = std::min(512, params.n_ctx / 2); j < params.n_ctx - 1; ++j) {
-            // Calculate probability of next token, given the previous ones.
-            const std::vector<float> tok_logits(
-                logits.begin() + (j + 0) * n_vocab,
-                logits.begin() + (j + 1) * n_vocab);
-
-            const float prob = softmax(tok_logits)[tokens[start + j + 1]];
-
-            nll += -std::log(prob);
-            ++count;
-        }
-        // perplexity is e^(average negative log-likelihood)
-        printf("[%d]%.4lf,", i + 1, std::exp(nll / count));
-        fflush(stdout);
-    }
-    printf("\n");
-}
-
-int main(int argc, char** argv) {
+int main(int argc, char ** argv) {
     gpt_params params;
 
     params.n_batch = 512;
@@ -162,8 +167,8 @@ int main(int argc, char** argv) {
 
     llama_backend_init(params.numa);
 
-    llama_model* model;
-    llama_context* ctx;
+    llama_model * model;
+    llama_context * ctx;
 
     // load the model and apply lora adapter, if any
     std::tie(model, ctx) = llama_init_from_gpt_params(params);
@@ -179,35 +184,7 @@ int main(int argc, char** argv) {
                 params.n_threads, std::thread::hardware_concurrency(), llama_print_system_info());
     }
 
-    std::vector<std::string> prompts = readPromptsFromFile("/kaggle/working/prompts.txt");
-
-    std::ofstream perplexityFile("/kaggle/working/perplexity.txt");
-    if (!perplexityFile) {
-        fprintf(stderr, "%s: error: unable to create perplexity file\n", __func__);
-        return 1;
-    }
-
-    for (const auto& prompt : prompts) {
-        double nll = 0.0;
-        int count = 0;
-
-        perplexity(ctx, params, prompt);
-
-        // Calculate perplexity using the updated values of nll and count
-        for (int i = 0; i < n_chunk; ++i) {
-            // ...
-            for (int j = std::min(512, params.n_ctx / 2); j < params.n_ctx - 1; ++j) {
-                // ...
-                nll += -std::log(prob);
-                ++count;
-            }
-        }
-
-        perplexityFile << std::exp(nll / count) << std::endl;
-    }
-
-    perplexityFile.close();
-
+    perplexity(ctx, params);
 
     llama_print_timings(ctx);
     llama_free(ctx);
